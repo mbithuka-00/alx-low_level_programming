ad4f9cf590d27c3a74b01958bbbfc3c66b8d75ae
mbithuka-00 <mbithukamatthew@gmail.com>
2022-06-15 05:22:34 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
 /*
- *  * File: 4-puts.c
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- */   
+ * File: 4-puts.c
+ * Auth: mbithuka-00
+ */
 
 #include <stdio.h>
 
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- */      
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-}			
+	return (0);
+}	
