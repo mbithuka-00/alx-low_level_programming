cddee839684e61ed7f09c83ecdc2f1e936c0623d
mbithuka-00 <mbithukamatthew@gmail.com>
2022-06-15 06:18:35 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 /*
  *  * File: 0-positive_or_negative.c
  *   * Auth: mbithuka-00
- *    */
+ */
 
 #include <stdlib.h>
 #include <time.h>
@@ -12,7 +12,7 @@
  *   *        it is positive, negative, or zero.
  *    *
  *     * Return: Always 0.
- *     */
+ */
 int main(void)
 {
 		int n;
