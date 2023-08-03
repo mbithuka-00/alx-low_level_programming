e8383ea2e04a4d7d200fd3225052c4a97b82870b
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-03 09:27:46 -0700

    my commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of the decimal number
- * @n: number to print into binary
+ * print_binary - it  prints the binary representation
+ * of the number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  * AUTHOR: MATT MBITHUKA
  */
 void print_binary(unsigned long int n)
 {
-	int m;
-	unsigned long int mask = 1UL << 63;
-
-	for (m = 0; m < 64; m++)
+	if (n >> 0)
 	{
-		if (n & mask)
-			_putchar('1');
-		else if (m > 0)
-			_putchar('0');
-		mask >>= 1;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
-
